d308f3cc953c02cb52708f601ebde3da25c7f915
Ilya Stepanko <ilya@aylanetworks.com>
2016-02-01 17:03:43 -0800

    commit

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup
 
 setup(
-    name='test-rail-nose-plugin',
+    name='noserail',
     version='0.1.0',
     description='Nose plugin to report test results into TestRail',
+    py_modules=['plugin'],
     author='Ilya Stepanko',
     author_email='ilya.email@icloud.com',
     entry_points={
         'nose.plugins.0.10': [
-            'nose_testrail = nose_testrail.plugin:NoseTestRail'
+            'noserail = nose_testrail.plugin:NoseTestRail'
         ]
     },
 )
