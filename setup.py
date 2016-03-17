cbfafe07c09734cab94397079d798ad3edebf245
Ilya Stepanko <ilya@aylanetworks.com>
2016-03-16 18:28:09 -0700

    12321

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='noserail',
     version='0.1.0',
     description='Nose plugin to report test results into TestRail',
-    py_modules=['plugin'],
     author='Ilya Stepanko',
     author_email='ilya.email@icloud.com',
+    packages=find_packages(exclude=["tests"]),
     entry_points={
         'nose.plugins.0.10': [
             'noserail = noserail.plugin:NoseTestRail'
