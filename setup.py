0199882f08786285cd7745d6cf520951dee11d7a
Ilya Stepanko <ilya.email@icloud.com>
2016-03-23 18:29:29 -0700

    bumping up the version and testing some stuff

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='noserail',
-    version='0.1.0',
+    version='0.2.0',
     description='Nose plugin to report test results into TestRail',
     author='Ilya Stepanko',
     author_email='ilya.email@icloud.com',
